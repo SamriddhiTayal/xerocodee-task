180e5d532f39cdbd154a0f5efa90d99acc1a9c3a
Samriddhi Tayal <samriddhitayal@gmail.com>
2023-10-04 00:11:28 +0530

    customized onboarding form

--- src/components/Purpose.jsx
+++ src/components/Purpose.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 function Purpose() {
+	const [placeholder, setPlaceholder] = useState('');
 	return (
 		<div className="bg-[url('/assets/signup-bg.svg')] h-screen flex items-center justify-center">
 			<div className='flex flex-col items-center   bg-white rounded-3xl h-3/4 w-9/12'>
@@ -17,28 +20,40 @@ function Purpose() {
 					</div>
 				</div>
 				<div className='flex justify-around w-full pt-16'>
-					<button className='border border-[#C0C0C0] rounded text-[#000022] font-semibold text-base px-16 py-3'>
+					<button
+						className='border border-[#C0C0C0] rounded text-[#000022] font-semibold text-base px-16 py-3'
+						onClick={() => {
+							setPlaceholder('');
+						}}>
 						Developer
 					</button>
-					<button className='border border-[#C0C0C0] rounded text-[#000022] font-semibold text-base px-16 py-3'>
+					<button
+						className='border border-[#C0C0C0] rounded text-[#000022] font-semibold text-base px-16 py-3'
+						onClick={() => {
+							setPlaceholder('Organisation');
+						}}>
 						Organisation
 					</button>
-					<button className='border border-[#C0C0C0] rounded text-[#000022] font-semibold text-base px-16 py-3'>
+					<button
+						className='border border-[#C0C0C0] rounded text-[#000022] font-semibold text-base px-16 py-3'
+						onClick={() => {
+							setPlaceholder('Company');
+						}}>
 						Company
 					</button>
 				</div>
-				<form
-					action=''
-					className='p-20'>
-					<input
-						type='text'
-						placeholder='Organisation Name'
-						className='border border-gray-200 rounded p-3 pr-32 m-1 text-sm'
-					/>
-					<button className='bg-[#1F64FF] rounded text-white font-semibold p-3 px-4 m-1 text-sm'>
-						SUMIT
-					</button>
-				</form>
+				{placeholder !== '' ? (
+					<form className='p-20'>
+						<input
+							type='text'
+							placeholder={`${placeholder} Name`}
+							className='border border-gray-200 rounded p-3 pr-32 m-1 text-sm'
+						/>
+						<button className='bg-[#1F64FF] rounded text-white font-semibold p-3 px-4 m-1 text-sm'>
+							SUMIT
+						</button>
+					</form>
+				) : null}
 			</div>
 		</div>
 	);
